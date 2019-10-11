2ce32ee3c403831c39dfd1b1946a7c7a1cf76bd0
Gustaf Hultgren <gustaf.hultgren@byon8.com>
2019-10-11 16:37:43 +0200

    add meal item

--- lib/meal_item.dart
+++ lib/meal_item.dart
@@ -34,13 +34,35 @@ class MealItem extends StatelessWidget {
                   ),
                   child: Image.network(
                     imageUrl,
-                    height: 250,
+                    height: 300,
                     width: double.infinity,
                     fit: BoxFit.cover,
                   ),
+                ),
+                Positioned(
+                  bottom: 20,
+                  right: 10,
+                  child: Container(
+                    width: 220,
+                    color: Colors.black54,
+                    padding: EdgeInsets.symmetric(
+                      vertical: 5,
+                      horizontal: 20,
+                    ),
+                    child: Text(
+                      title,
+                      style: TextStyle(
+                        fontSize: 26,
+                        color: Colors.white,
+                      ),
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
+                    ),
+                  ),
                 )
               ],
-            )
+            ),
+            Padding(padding: EdgeInsets.all(20), child: Row(),),
           ],
         ),
       ),
